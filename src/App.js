96e73891d9ad909fb50b35d25517a042b012df4f
rxnaij <richardblue37@gmail.com>
2019-07-19 23:17:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const defaultTextColor = '#000';
+const defaultStyle = {
+  color: defaultTextColor,
+  
+};
+
+class Aggregate extends Component {
+  render() {
+    return(
+      <div style={ {...defaultStyle, width: '40%', display: 'inline-block'} } >
+        <h2 style={ {defaultStyle} }>Number: Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style={ {defaultStyle} }>
+        <img alt="Filter" />
+        <input type="text" name="" id=""/>
+        Filter
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+      <div style={ {...defaultStyle, width: '25%', display: 'inline-block'} }>
+        <img src="" alt=""/>
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
+}
+
+export default App;
